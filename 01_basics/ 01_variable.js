ed6141bec71cf95c6745f6980d93aaaf74742f3e
ShubhamPandey121 <shubham230884@gmail.com>
2024-12-23 08:43:52 +0000

    learned const and let

---
+++ 01_basics/ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "shubham@google.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+let accountState;
+console.table([accountPassword,accountEmail,accountId,accountState,accountCity])
+accountId =3
+
+accountEmail ="shubham@yahoo.com"
+accountPassword ="1212121212"
+accountCity = "Bengaluru"
+console.table([accountPassword,accountEmail,accountId,accountState,accountCity])
