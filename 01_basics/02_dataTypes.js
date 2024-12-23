54a52de631b72fb3da85ed83a46e528e573f3f7d
ShubhamPandey121 <shubham230884@gmail.com>
2024-12-23 12:36:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using node JS not browser
+console.log(3+3) // in method JS avoid spaces and new line
+console.log("shubham")    // code readibility should be high
+
+let name="Inida"
+let number=121
+let isLogged =true
+let state;
+
+//number => 2 to power 53
+// bigint
+// boolean=> true/false
+//string =>"" or'' ,but we will prefer ""
+// null => standalone value [iska matlab hai ki ya container khali hai]
+// undefined => its data type is "undefined" [ iska matlab hota hai abhi value assign nhi ki gayi hai]
+// in short null is defined but its value is null whereas undefined toh undefined hai
+
+// symbol => unique [when we have to compare component and have to find which is unique ]
+/* primitive data type over */
+
+// object
+
+console.log(typeof 123.45)
+console.log(typeof undefined) // undefined 
+console.log(typeof null)    // object
+
+
