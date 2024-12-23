18296a2e592403291df2b82d945c55446e133992
ShubhamPandey121 <shubham230884@gmail.com>
2024-12-23 08:45:01 +0000

    learned const and let

--- 01_basics/ 01_variable.js
+++ 01_basics/ 01_variable.js
@@ -10,3 +10,11 @@ accountEmail ="shubham@yahoo.com"
 accountPassword ="1212121212"
 accountCity = "Bengaluru"
 console.table([accountPassword,accountEmail,accountId,accountState,accountCity])
+
+// huma sirf let use krna variable conatiner ka liya
+//var and let dono use ho skta hai but var and let mai ya difference hai ki ,let is updated version of var
+// kuch nhi use karengai toh woh bhi variable he consider kara jayega 
+
+/* prefer not to use var 
+because of issue in block scope and functional scope
+*/
